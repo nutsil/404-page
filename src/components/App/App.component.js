adbdc28d9fa45b81adeddc615436b7ed551e219f
nutsil <silpagui.sa@gmail.com>
2020-10-15 18:27:52 -0300

    Define loyout

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -1,5 +1,15 @@
 import React from "react";
+import "./App.styles.scss";
 
 export function App() {
-  return <div></div>;
+  return (
+    <div className="app-wrapper">
+      <div className="header"></div>
+      <div className="area-2">
+        <div className="area-2-a"></div>
+        <div className="area-2-b"></div>
+      </div>
+      <div className="area-3"></div>
+    </div>
+  );
 }
