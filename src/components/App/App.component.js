0205ff355121ca2b95c4a6b3162ff9ceb492c988
nutsil <silpagui.sa@gmail.com>
2020-10-19 17:58:10 -0300

    Add TitlePage component to Header in App component

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { TitlePage } from "../TitlePage/TitlePage.component";
 import "./App.styles.scss";
 
 export function App() {
   return (
     <div className="app-wrapper">
-      <div className="header"></div>
+      <div className="header">
+        <TitlePage />
+      </div>
       <div className="area-2">
         <div className="area-2-a"></div>
         <div className="area-2-b"></div>
