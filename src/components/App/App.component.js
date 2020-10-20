6a9b741be9b6f984e8c3f8c4c261139ebd01db2f
nutsil <silpagui.sa@gmail.com>
2020-10-20 17:41:03 -0300

    Add Scarecrow.png file to area 2-A

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { TitlePage } from "../TitlePage/TitlePage.component";
 import "./App.styles.scss";
+import ScareCrow from "../../assets/images/Scarecrow.png";
 
 export function App() {
   return (
@@ -9,7 +10,15 @@ export function App() {
         <TitlePage />
       </header>
       <main className="area-2">
-        <div className="area-2-a"></div>
+        <div className="area-2-a">
+          <img
+            src={ScareCrow}
+            alt="scarecrow"
+            width="286"
+            height="238"
+            className="scarecrow-image"
+          />
+        </div>
         <div className="area-2-b"></div>
       </main>
       <footer className="area-3"></footer>
