8a0f52ad3b335e81d6f4496a28dfc6e9771d58c9
nutsil <silpagui.sa@gmail.com>
2020-10-29 18:11:47 -0300

    Rename folder

---
+++ src/components/Copyright/Copyright.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Copyright.styles.scss";
+
+export function Copyright() {
+  return (
+    <footer className="copyright-text">Your name @ DevChallenges.io</footer>
+  );
+}
